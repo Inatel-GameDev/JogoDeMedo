e4c2275acbc47af66c2bb17ec0a0961ddbe4a43c
dju4mota <fernando.b@ges.inatel.br>
2025-04-28 19:06:36 -0300

    UI base do inventário

--- Medo/Assets/Scripts/Jogador/InventoryUI.cs
+++ Medo/Assets/Scripts/Jogador/InventoryUI.cs
@@ -1,30 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
-    [SerializeField] private Transform itemsParent;
-    [SerializeField] private GameObject itemSlotPrefab;
 
-    private Inventario inventory;
-
-    private void Start()
+    public Image[] slots = new Image[4];
+    public Inventario inventory;
+    
+    public void UpdateUI()
     {
-        inventory = GetComponent<Inventario>();
-        UpdateUI();
-    }
-
-    private void UpdateUI()
-    {
-        foreach(Transform child in itemsParent)
+        for (int i = 0; i < slots.Length; i++)
         {
-            Destroy(child.gameObject);
+            slots[i].sprite = null;
         }
-
-        foreach(Item item in inventory.GetItems())
+        
+        for (int i = 0; i < inventory.GetItems().Count; i++)
         {
-            GameObject slot = Instantiate(itemSlotPrefab, itemsParent);
-            slot.GetComponent<Image>().sprite = item.icon;
+            slots[i].sprite = inventory.GetItems()[i].icon;
         }
     }
 }
